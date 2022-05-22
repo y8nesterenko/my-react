b26091ce42ab83c7c07a96844319e14e5b6f738d
y8nesterenko <y8nesterenko@gmail.com>
2022-05-22 18:29:10 +0300

    if-else modified to switch-case

--- src/redux/state.js
+++ src/redux/state.js
@@ -24,19 +24,51 @@ let store = {
         messagesPage: {
 
             dialogs: [
-                {id: 1, name: "Вася", avatar: "https://www.meme-arsenal.com/memes/f95856ab40a1b549bfa2f1514099b2c7.jpg"},
-                {id: 2, name: "Петя", avatar: "https://www.meme-arsenal.com/memes/c7dc460f61771f649ee17facc185ef3a.jpg"},
-                {id: 3, name: "Коля", avatar: "https://www.meme-arsenal.com/memes/51a972709e645d4f9a8a10afe540c567.jpg"},
-                {id: 4, name: "Антон", avatar: "https://www.meme-arsenal.com/memes/f76041521ee067dce455716fec5f0560.jpg"},
-                {id: 5, name: "Иван", avatar: "https://www.meme-arsenal.com/memes/a3e01e1426a1c0b290b40d01ceadfed8.jpg"},
-                {id: 6, name: "Фрол", avatar: "https://www.meme-arsenal.com/memes/242569be9a3b25fa0e0704d0b088c33a.jpg"},
-                {id: 7, name: "Арнольд", avatar: "https://www.meme-arsenal.com/memes/c00ac4f98c08a5cc0924ba812415d27e.jpg"},
+                {
+                    id: 1,
+                    name: "Вася",
+                    avatar: "https://www.meme-arsenal.com/memes/f95856ab40a1b549bfa2f1514099b2c7.jpg"
+                },
+                {
+                    id: 2,
+                    name: "Петя",
+                    avatar: "https://www.meme-arsenal.com/memes/c7dc460f61771f649ee17facc185ef3a.jpg"
+                },
+                {
+                    id: 3,
+                    name: "Коля",
+                    avatar: "https://www.meme-arsenal.com/memes/51a972709e645d4f9a8a10afe540c567.jpg"
+                },
+                {
+                    id: 4,
+                    name: "Антон",
+                    avatar: "https://www.meme-arsenal.com/memes/f76041521ee067dce455716fec5f0560.jpg"
+                },
+                {
+                    id: 5,
+                    name: "Иван",
+                    avatar: "https://www.meme-arsenal.com/memes/a3e01e1426a1c0b290b40d01ceadfed8.jpg"
+                },
+                {
+                    id: 6,
+                    name: "Фрол",
+                    avatar: "https://www.meme-arsenal.com/memes/242569be9a3b25fa0e0704d0b088c33a.jpg"
+                },
+                {
+                    id: 7,
+                    name: "Арнольд",
+                    avatar: "https://www.meme-arsenal.com/memes/c00ac4f98c08a5cc0924ba812415d27e.jpg"
+                },
                 {
                     id: 8,
                     name: "Товарищь",
                     avatar: "https://www.meme-arsenal.com/memes/c79d46e696c06b0bc0cf81ff08d1f44f.jpg"
                 },
-                {id: 9, name: "Тутси", avatar: "https://www.meme-arsenal.com/memes/56137e53246061fe6a57421c809f17f1.jpg"},
+                {
+                    id: 9,
+                    name: "Тутси",
+                    avatar: "https://www.meme-arsenal.com/memes/56137e53246061fe6a57421c809f17f1.jpg"
+                },
             ],
 
             messages: [
@@ -84,7 +116,8 @@ let store = {
             ],
         },
     },
-    _callSubscriber() {},
+    _callSubscriber() {
+    },
     getState() {
         return this._state;
     },
@@ -102,15 +135,11 @@ let store = {
             this._state.profilePage.posts.push(newPost);
             this._state.profilePage.newPostText = '';
             this._callSubscriber(this._state);
-        }
-
-        else if (action.type === UPDATE_NEW_POST_TEXT) {
+        } else if (action.type === UPDATE_NEW_POST_TEXT) {
             //Текст нового поста приходит нам из екшена (поля ввода пользователем)
             this._state.profilePage.newPostText = action.newText;
             this._callSubscriber(this._state);
-        }
-
-        else if (action.type === ADD_MESSAGE) {
+        } else if (action.type === ADD_MESSAGE) {
             //Создаём объект, который будем добавлять в массив
             let newMessage = {
                 id: 10,
@@ -122,23 +151,35 @@ let store = {
             //Зануляем содержимое поля ввода
             this._state.messagesPage.newMessageText = '';
             this._callSubscriber(this._state);
-        }
-
-        else if (action.type === UPDATE_NEW_MESSAGE_TEXT) {
+        } else if (action.type === UPDATE_NEW_MESSAGE_TEXT) {
             this._state.messagesPage.newMessageText = action.newMessage;
             this._callSubscriber(this._state);
         }
 
-        //равноценназапись через Switch
+        /*равноценная запись через Switch
 
-        // switch (action.type)
-        //     case: ADD_POST, ;lm smlkmsklvdskl nklvsnklvsnklsvnklvsklvsdmklvsmkl
-
-        },
+        switch (action.type) {
+            case ADD_POST:
+                let newPost = {
+                    id: 5,
+                    message: this._state.profilePage.newPostText,
+                    like: 0,
+                };
+                this._state.profilePage.posts.push(newPost);
+                this._state.profilePage.newPostText = '';
+                this._callSubscriber(this._state);
+            case UPDATE_NEW_MESSAGE_TEXT:
+                this._state.messagesPage.newMessageText = action.newMessage;
+                this._callSubscriber(this._state);
+            default: this._callSubscriber(this._state);
+        }
+        */
+    },
 };
 export const addPostActionCreator = () => {
     return {
-        type: ADD_POST};
+        type: ADD_POST
+    };
 };
 // Равнозначная запись
 //export const addPostActionCreator = () => ({type: ADD_POST});
@@ -146,7 +187,8 @@ export const addPostActionCreator = () => {
 export const addNewPostTextActionCreator = (text) => {
     return {
         type: UPDATE_NEW_POST_TEXT,
-        newText: text};
+        newText: text
+    };
 };
 
 export const addMessageActionCreator = () => ({type: ADD_MESSAGE});
