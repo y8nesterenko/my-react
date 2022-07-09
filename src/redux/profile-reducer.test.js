d41e5714227e413d46cd1a3a6d0e40581c1b27e8
y8nesterenko <y8nesterenko@gmail.com>
2022-07-09 15:57:03 +0300

    89 TDD, created profile-reducer.test.js

---
+++ src/redux/profile-reducer.test.js
@@ -0,0 +1,40 @@
+import profileReducer, {addPostActionCreator, deletePost} from "./profile-reducer";
+
+//1. start test data
+let state = {
+    posts: [
+        {id: 1, message: "Это мой первый пост", like: 100},
+        {id: 2, message: "Это мой второй пост", like: 200},
+        {id: 3, message: "Это мой третий пост", like: 300},
+    ]
+};
+
+it('length of posts should be incremented', () => {
+
+    //1. start test data
+    let action = addPostActionCreator('новый пост');
+
+    //2. action
+    let newState = profileReducer(state, action);
+
+    //3. expectation
+    expect(newState.posts.length).toBe(4);
+});
+
+it('correct text of newText', () => {
+    let action = addPostActionCreator('новый пост');
+    let newState = profileReducer(state, action);
+    expect(newState.posts[2].message).toBe('новый пост');
+});
+
+it('after deleting length of messages should be incremented', () => {
+    let action = deletePost(3);
+    let newState = profileReducer(state, action);
+    expect(newState.posts.length).toBe(2);
+});
+
+it("after deleting post with wrong id length of messages shouldn't be changed", () => {
+    let action = deletePost(4);
+    let newState = profileReducer(state, action);
+    expect(newState.posts.length).toBe(3);
+});
