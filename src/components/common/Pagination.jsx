6dd2350e42f90780bbc2a4060944916e83d8cacb
y8nesterenko <y8nesterenko@gmail.com>
2022-07-11 12:58:22 +0300

    93 Pagination refactoring

--- src/components/common/Pagination.jsx
+++ src/components/common/Pagination.jsx
@@ -13,26 +13,49 @@ const Pagination = (props) => {
     }
     ;
 
-    //вывод части старниц в пагинации
-    let curPF = ((props.currentPage - 5) < 0) ? 0 : props.currentPage - 5;
-    let curPL = props.currentPage + 5;
+//вывод части старниц в пагинации
+    let curPF = ((props.currentPage - 3) < 0) ? 0 : props.currentPage - 3;
+    let curPL = props.currentPage + 3;
     let slicedPages = pages.slice(curPF, curPL);
 
     return (
         <div>
             {/*выводим кнопки пагинации для отображения пользователей*/}
             <div className={style.pagination}>
+                <button onClick={(e) => {
+                    props.onPageChanged(pages[0]);
+                }}>start
+                </button>
+
+                <button disabled={props.currentPage < 6}
+                    onClick={(e) => {
+                    props.onPageChanged(props.currentPage - 5);
+                }}>prev
+                </button>
+
                 {/*пробегаемся по количеству страниц в массиве и отображаем каждую в отдельной кнопке*/}
-                {slicedPages.map(p => {
+                {slicedPages.map(page => {
                     //стрелочная функция всегда должна что-то возвращать (return'ить)
-                    return <button key={p.id}
+                    return <button key={page.id}
                         //если номер текущей страницы равен номеру страницы, тогда выделяем её в css
-                                   className={props.currentPage === p && style.selectedPage}
+                                   className={props.currentPage === page && style.selectedPage}
                         //при клиеке меняем текущую страницу (currentPage) в state'е
                                    onClick={(e) => {
-                                       props.onPageChanged(p);
-                                   }}>{p}</button>
+                                       props.onPageChanged(page);
+                                   }}>{page}</button>
                 })}
+
+                <button disabled={props.currentPage > pages.length - 5}
+                    onClick={(e) => {
+                        props.onPageChanged(props.currentPage + 5);
+                    }}>next
+                    </button>
+
+                <button onClick={(e) => {
+                    props.onPageChanged(pages.length);
+                }}>end
+                </button>
+
             </div>
         </div>
     )
